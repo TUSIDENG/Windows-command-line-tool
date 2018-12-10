5ff62a858c55dfa718349a44ca0a1efec370918d
TUSIDENG <1561799709@qq.com>
2018-12-10 17:33:05 +0800

    工具:自动请求管理员权限

---
+++ cmd/tools/getAdmin.bat
@@ -0,0 +1,25 @@
+@echo off
+
+>nul 2>&1 "%SYSTEMROOT%\system32\icacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+if '%errorlevel%' NEQ '0' (
+
+echo 请求管理员权限...
+
+goto UACPrompt
+
+) else ( goto gotAdmin )
+
+:UACPrompt
+
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+"%temp%\getadmin.vbs"
+
+exit /B
+
+:gotAdmin
+echo 已得到管理身份
+pause
